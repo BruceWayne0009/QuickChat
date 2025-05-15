d1526c571e28dc546547dad722df9a7b4c195184
BruceWayne0009 <maayush2005@gmail.com>
2025-05-15 13:58:46 +0530

    added search bar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import assets from '../assets/assets'
+import assets, { userDummyData } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 
 const Sidebar = ({selectedUser, setelectedUser}) => {
@@ -20,7 +20,22 @@ const Sidebar = ({selectedUser, setelectedUser}) => {
                     </div>
                 </div>
             </div>
+
+            <div className='bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4 mt-5'>
+                <img src={assets.search_icon} alt="Search"  className='w-3'/>
+                <input type="text" className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1' placeholder='Search User....'/>
+            </div>
+
         </div>
+
+        <div className='flex flex-col'>
+        {userDummyData.map((user, index) => (
+            <div>
+                <img src={user?.profilePic || assets.avatar_icon} alt="user icon" className='w-[35px] aspect-[1/1] rounded-full'/>
+            </div>
+        ))}
+        </div>
+
     </div>
   )
 }
